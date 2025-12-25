06ffbcb53c913d5cd84175b5a3db345420eea12c
shirshadip <artistavra@gmail.com>
2025-12-25 19:24:08 +0530

    bug fixing

--- code/code.ino
+++ code/code.ino
@@ -1,32 +1,31 @@
 int ldrPin = A0;
-int redPin = 2;
+int bluePin = 2;
 int greenPin = 3;
-int bluePin = 4;
+int redPin = 4;
 
 void setup() {
   Serial.begin(9600);
   analogReadResolution(10);
+
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
-
 }
 
 void loop() {
   int ldr = analogRead(ldrPin);
-  // Serial.println(ldr);
   delay(300);
 
   if (ldr < 900) {   // DARK
-    digitalWrite(redPin, HIGH);
-    digitalWrite(greenPin, HIGH);
-    digitalWrite(bluePin, HIGH);
-    Serial.println("Light is off");
-  } else {
-    digitalWrite(redPin, LOW);
-    digitalWrite(greenPin, LOW);
-    digitalWrite(bluePin, LOW);
-    Serial.println("Light is on");
-  }
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, HIGH);
+  digitalWrite(bluePin, HIGH);
+  Serial.println("Dark → Light is off");
+} else {
+  digitalWrite(redPin, LOW);
+  digitalWrite(greenPin, LOW);
+  digitalWrite(bluePin, LOW);
+  Serial.println("Bright → Light on");
 }
 
+}
